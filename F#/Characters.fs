abce1b3c0855559810f2eea3f6120c521e8fca3b
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-02-14 23:28:54 +0600

    added charcters example on F#

---
+++ F#/Characters.fs
@@ -0,0 +1,39 @@
+﻿open LiliumEngine
+open LiliumEngine.Basics
+open LiliumEngine.UI
+open LiliumEngine.UI.Elements
+open LiliumEngine.Managment
+open LiliumEngine.Audio
+
+[<Literal>]
+let EndGame = 0
+
+// General:
+let mainColor = new ColorUI(154uy, 8uy, 66uy)
+let game = new Game("Title", @"<font file path>", mainColor)
+let audio = new AudioPlayer(game)
+let story = new StoryTelling(game)
+
+// Characters:
+let bob = new Character(game, "Bob")
+let alice = new Character(game, "Alice")
+
+let setup() =
+    
+    let startButton = new Button(840f, 700f, "Play", mainColor, 30u)
+    startButton.Click.Add(fun evArgs -> SceneManager.LoadScene(game, "Main"))
+
+    game.AddElementsUI("Menu", startButton)
+
+[<EntryPoint>]
+let main args =
+    setup()
+    
+    // outputs a message in the format - characterName: "message"
+    bob.Say "Nice to meet you, I'm Bob"
+
+    alice.Say "Hello, I'm Alice"
+
+    game.Run()
+
+    EndGame
