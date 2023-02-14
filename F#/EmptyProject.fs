3bed6607ec47f62c92b0c2dddae1a559ede420ea
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-02-14 21:27:38 +0600

    Добавьте файлы проекта.

---
+++ F#/EmptyProject.fs
@@ -0,0 +1,26 @@
+﻿open LiliumEngine
+open LiliumEngine.Basics
+open LiliumEngine.UI
+open LiliumEngine.UI.Elements
+open LiliumEngine.Managment
+open LiliumEngine.Audio
+
+[<Literal>]
+let EndGame = 0
+
+// General:
+let mainColor = new ColorUI(0uy, 0uy, 0uy)
+let game = new Game("Title", @"<font file path>", mainColor)
+let audio = new AudioPlayer(game)
+let story = new StoryTelling(game)
+
+let setup() =
+    0
+
+[<EntryPoint>]
+let main args =
+    setup()
+
+    game.Run()
+
+    EndGame
