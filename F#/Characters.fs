7b3e011a248de972507a3b67bf521e7b3c918352
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-10-29 16:51:02 +0600

    Update Characters.fs

--- F#/Characters.fs
+++ F#/Characters.fs
@@ -9,12 +9,16 @@ open LiliumEngine.Audio
 let EndGame = 0
 
 // General:
+// Жалпы:
+// Общий:
 let mainColor = new ColorUI(154uy, 8uy, 66uy)
 let game = new Game("Title", @"<font file path>", mainColor)
 let audio = new AudioPlayer(game)
 let story = new StoryTelling(game)
 
 // Characters:
+// Таңбалар:
+// Персонажи
 let bob = new Character(game, "Bob")
 let alice = new Character(game, "Alice")
 
@@ -29,9 +33,13 @@ let setup() =
 let main args =
 
     // !!! THE FOLLOWING METHODS SHOULD BE CALLED BEFORE game.Run()
+    // !!! ТӨМЕНДЕГІ ӘДІСТЕР game.Run() БҰРЫН ШАҚЫРУ КЕРЕК
+    // !!! СЛЕДУЮЩИЕ МЕТОДЫ ДОЛЖНЫ БЫТЬ ВЫЗОВАНЫ ПЕРЕД game.Run()
     setup()
     
     // outputs a message in the format - characterName: "message"
+    // хабарламаны форматта шығарады - characterName: "хабарлама"
+    //выводит сообщение в формате - characterName:"message"
     bob.Say "Nice to meet you, I'm Bob"
 
     alice.Say "Hello, I'm Alice"
