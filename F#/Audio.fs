09e00703c342073c01681aa0ffda234ca7128b58
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-10-29 16:42:46 +0600

    Update Audio.fs

--- F#/Audio.fs
+++ F#/Audio.fs
@@ -9,6 +9,8 @@ open LiliumEngine.Audio
 let EndGame = 0
 
 // General:
+// Жалпы:
+// Общий:
 let mainColor = new ColorUI(154uy, 8uy, 66uy)
 let game = new Game("Title", @"<font file path>", mainColor)
 let audio = new AudioPlayer(game)
@@ -25,12 +27,20 @@ let setup() =
 let main args =
 
     // !!! THE FOLLOWING METHODS SHOULD BE CALLED BEFORE game.Run()
+    // !!! ТӨМЕНДЕГІ ӘДІСТЕР Game.Run() БҰРЫН ШАҚЫРУ КЕРЕК
+    // !!! СЛЕДУЮЩИЕ МЕТОДЫ ДОЛЖНЫ БЫТЬ ВЫЗОВАНЫ ПЕРЕД игрой.Run()
     setup()
     
     story.Write "And suddenly the music started playing..."
 
     // Launch music on the specified file. The volume is set from 0 to 100.
     // As well as the looping flag (whether the song will be played again after the end), by default False.
+
+    // Көрсетілген файлда музыканы іске қосыңыз. Дыбыс деңгейі 0-ден 100-ге дейін орнатылған.
+     // Сонымен қатар циклдік жалауша (ән аяқталғаннан кейін қайта ойнатылады ма), әдепкі бойынша False.
+
+     // Запускаем музыку из указанного файла. Громкость устанавливается от 0 до 100.
+     // А также флаг зацикливания (будет ли песня воспроизводиться снова после окончания), по умолчанию False.
     audio.PlayMusic(@"music file path", 100, true)
 
     game.Run()
