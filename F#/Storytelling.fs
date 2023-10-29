e2ed1b3a41dd23b99747873b3cf1738d08a17905
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-10-29 17:08:03 +0600

    Update Storytelling.fs

--- F#/Storytelling.fs
+++ F#/Storytelling.fs
@@ -27,21 +27,35 @@ let setup() =
 let main args =
 
     // !!! THE FOLLOWING METHODS SHOULD BE CALLED BEFORE game.Run()
+    // !!! ТӨМЕНДЕГІ ӘДІСТЕР game.Run() БҰРЫН ШАҚЫРУ КЕРЕК
+    // !!! СЛЕДУЮЩИЕ МЕТОДЫ ДОЛЖНЫ БЫТЬ ВЫЗОВАНЫ ПЕРЕД game.Run()
     setup()
     
     // Displays impersonal text without quotes. Can be used as the words of the author or thoughts of the hero
+    // Жеке емес мәтінді тырнақшасыз көрсетеді. Автордың сөзі немесе кейіпкердің ойы ретінде қолдануға болады
+    // Отображает обезличенный текст без кавычек. Могут быть использованы как слова автора, так и мысли героя.
     story.Write "This is the text of the author - will be shown without quotes."
 
     // Stops updating the dialog thread for the specified number of seconds
+    // Диалогтың ағынының жаңартылуын көрсетілген секунд санына тоқтатады
+    // Останавливает обновление диалогового потока на указанное количество секунд
     story.WaitFor 10u
 
     story.Write "You've waited for 10 seconds."
 
     // Loads an illustration into the scene from the specified file
+    // Көрсетілген файлдан көрініске иллюстрацияны жүктейді
+    // Загружает иллюстрацию в сцену из указанного файла
     story.LoadIllustration @"<image file path>"
 
     // Displays options on the screen. Variants are written on one line and separated by "|".
     // This is followed by the definitions of the lambda function, respectively, for each
+
+    // Экранда опцияларды көрсетеді. Варианттар бір жолға жазылады және «|» арқылы бөлінеді.
+    // Осыдан кейін әрқайсысы үшін сәйкесінше ламбда функциясының анықтамалары келеді
+
+    // Отображает параметры на экране. Варианты пишутся в одну строку и разделяются знаком «|».
+    // Далее следуют определения лямбда-функции соответственно для каждого
     story.ShowMenu("First|Second",
         fun() -> story.Write "You clicked on the first!"
         ,
