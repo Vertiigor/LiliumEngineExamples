5fb77a150f61e4db069f29d7d9ecd8fd696dd222
Igor Makowski <88109966+Vertiigor@users.noreply.github.com>
2023-10-29 16:58:28 +0600

    Update EmptyProject.fs

--- F#/EmptyProject.fs
+++ F#/EmptyProject.fs
@@ -9,9 +9,24 @@ open LiliumEngine.Audio
 let EndGame = 0
 
 // General:
+// Жалпы:
+// Общий:
+
 let mainColor = new ColorUI(0uy, 0uy, 0uy)
+
+// main variable, which is responsible for initializing and starting the game. Constructor includes(game's name, font file, UI's default color)
+// ойынды инициализациялауға және бастауға жауап беретін негізгі айнымалы. Конструктор кіреді (ойынның аты, қаріп файлы, UI әдепкі түсі)
+// основная переменная, отвечающая за инициализацию и запуск игры. Конструктор включает в себя (название игры, файл шрифта, цвет пользовательского интерфейса по умолчанию)
 let game = new Game("Title", @"<font file path>", mainColor)
+
+// variable responsible for interaction with the audio system
+// аудио жүйемен өзара әрекеттесу үшін жауапты айнымалы
+// переменная, отвечающая за взаимодействие с аудиосистемой
 let audio = new AudioPlayer(game)
+
+// variable responsible for controlling the plot and design
+// сюжетті және дизайнды басқаруға жауапты айнымалы
+// переменная, отвечающая за управление сюжетом и дизайном
 let story = new StoryTelling(game)
 
 let setup() =
